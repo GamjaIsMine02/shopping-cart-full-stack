7ec9151fc8e6e6b8c226d3cad7e25041dbef1850
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-06-05 00:26:17 +0900

    feat: 비동기 상태에 따라 컴포넌트 보여주는 로직 구현 & 폼 제출 기능 구현

--- client/src/feature/Cart/CartPage.tsx
+++ client/src/feature/Cart/CartPage.tsx
@@ -1,16 +1,67 @@
-import { Button } from '../../common/components/Button';
 import { Header } from '../../common/components/Header';
 import { CartItemListSection } from './components/CartItemListSection';
+import { CartEmptyView } from './components/CartEmptyView';
+import { CartErrorView } from './components/CartErrorView';
 import { CartProvider } from './components/CartProvider';
+import { CartSkeleton } from './components/CartSkeleton';
 import { CartSummary } from './components/CartSummary';
+import { useCartContext } from './context/CartContext';
+import { Button } from '../../common/components/Button';
 
 export const CartPage = () => {
   return (
     <CartProvider>
       <Header title="SHOP" />
+      <CartOrderForm />
+    </CartProvider>
+  );
+};
+
+// 페이지 이동을 위한 Form 컴포넌트
+const CartOrderForm = () => {
+  const { selectedCartItemIds, cartFetchStatus } = useCartContext();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // 결제 확인 페이지로 이동 & 필요한 데이터 보내기
+  };
+
+  const isOrderDisabled =
+    cartFetchStatus !== 'success' || selectedCartItemIds.length === 0;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CartContent />
+      <Button type="submit" disabled={isOrderDisabled}>
+        주문 확인
+      </Button>
+    </form>
+  );
+};
+
+const CartContent = () => {
+  const { cartItems, cartFetchStatus, cartFetchError, retryLoadCartItems } =
+    useCartContext();
+
+  if (cartFetchStatus === 'idle' || cartFetchStatus === 'loading') {
+    return <CartSkeleton />;
+  }
+
+  if (cartFetchStatus === 'error') {
+    return (
+      <CartErrorView error={cartFetchError} onRetry={retryLoadCartItems} />
+    );
+  }
+
+  if (cartItems.length === 0) {
+    return <CartEmptyView />;
+  }
+
+  return (
+    <>
       <CartItemListSection />
       <CartSummary />
-      {/* <Button /> */}
-    </CartProvider>
+    </>
   );
 };
