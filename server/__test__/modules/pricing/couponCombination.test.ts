f825451dd1444db055019934b91fde16884b7bf9
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-06-15 13:15:33 +0900

    test: 쿠폰 조합 생성 기능 구현 및 테스트 추가

---
+++ server/__test__/modules/pricing/couponCombination.test.ts
@@ -0,0 +1,86 @@
+import { CouponContext } from '../../../src/interfaces/couponPolicy.interface.js';
+import { createCouponCombinations } from '../../../src/utils/\bcouponCombination.js';
+import {
+  createBogoCoupon,
+  createContext,
+  createFixedAmountCoupon,
+  createFreeShippingCoupon,
+  createMiracleSaleCoupon,
+} from '../../helpers/createCoupons.js';
+
+describe('couponCombination', () => {
+  test('쿠폰 목록으로 0개, 1개, 2개 쿠폰 조합을 만든다', () => {
+    const coupons = [
+      createFixedAmountCoupon(),
+      createBogoCoupon(),
+      createFreeShippingCoupon(),
+      createMiracleSaleCoupon(),
+    ];
+
+    const combinations = createCouponCombinations(createContext(), coupons);
+
+    expect(combinations).toEqual([
+      [],
+      [coupons[0]],
+      [coupons[1]],
+      [coupons[2]],
+      [coupons[3]],
+      [coupons[0], coupons[1]],
+      [coupons[0], coupons[2]],
+      [coupons[0], coupons[3]],
+      [coupons[1], coupons[2]],
+      [coupons[1], coupons[3]],
+      [coupons[2], coupons[3]],
+    ]);
+  });
+
+  test('최대 2개까지만 쿠폰 조합을 만든다', () => {
+    const coupons = [
+      createFixedAmountCoupon(),
+      createBogoCoupon(),
+      createFreeShippingCoupon(),
+      createMiracleSaleCoupon(),
+    ];
+
+    const combinations = createCouponCombinations(createContext(), coupons);
+
+    const hasMoreThanTwoCoupons = combinations.some(
+      (combination) => combination.length > 2,
+    );
+
+    expect(hasMoreThanTwoCoupons).toBe(false);
+  });
+
+  test('쿠폰 목록에서 비활성화된 쿠폰은 제외하고 조합을 만든다', () => {
+    const coupons = [
+      createFixedAmountCoupon(), // 비활성화
+      createBogoCoupon(),
+      createFreeShippingCoupon(),
+      createMiracleSaleCoupon(), // 비활성화
+    ];
+
+    const context: CouponContext = {
+      orderProducts: [
+        {
+          productId: 'product-1',
+          productName: '상품A',
+          productPrice: 20000,
+          quantity: 3,
+        },
+      ],
+      orderPrice: 60000,
+      deliveryFee: 3000,
+      isIsland: false,
+      now: new Date('2026-06-14T10:00:00'),
+    };
+
+    const combinations = createCouponCombinations(context, coupons);
+
+    expect(combinations).toEqual([
+      [],
+      [coupons[1]],
+      [coupons[2]],
+      [coupons[1], coupons[2]],
+    ]);
+  });
+});
