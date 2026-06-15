b79367f38af0d0e390b3da954b092806fba50cd7
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-06-15 10:59:20 +0900

    test: 쿠폰 모델, 정책 테스트 작성 및 구현

---
+++ server/src/modules/coupons/coupons.model.ts
@@ -0,0 +1,188 @@
+export type CouponType = 'FIXED_AMOUNT' | 'BOGO' | 'FREE_SHIPPING' | 'RATE';
+
+export type CouponContext = {
+  orderProducts: {
+    productId: string;
+    productName: string;
+    productPrice: number;
+    quantity: number;
+  }[];
+  orderPrice: number;
+  deliveryFee: number;
+  isIsland: boolean;
+  now: Date;
+};
+
+export type CouponDiscount = {
+  productDiscountPrice: number;
+  deliveryDiscountPrice: number;
+};
+
+export interface CouponPolicy {
+  couponId: string;
+  expiresAt: Date;
+  type: CouponType;
+
+  isApplicable(context: CouponContext): boolean;
+  calculateDiscount(context: CouponContext): CouponDiscount;
+}
+
+type FixedAmountCouponParams = {
+  couponId: string;
+  expiresAt: Date;
+  minimumOrderPrice: number;
+  discountPrice: number;
+};
+
+export class FixedAmountCoupon implements CouponPolicy {
+  couponId;
+  expiresAt;
+  type = 'FIXED_AMOUNT' as const;
+
+  minimumOrderPrice;
+  discountPrice;
+
+  constructor(params: FixedAmountCouponParams) {
+    this.couponId = params.couponId;
+    this.expiresAt = params.expiresAt;
+
+    this.minimumOrderPrice = params.minimumOrderPrice;
+    this.discountPrice = params.discountPrice;
+  }
+
+  isApplicable(context: CouponContext) {
+    return (
+      context.now <= this.expiresAt &&
+      context.orderPrice >= this.minimumOrderPrice
+    );
+  }
+  calculateDiscount(context: CouponContext): CouponDiscount {
+    return {
+      productDiscountPrice: this.discountPrice,
+      deliveryDiscountPrice: 0,
+    };
+  }
+}
+
+type BogoCouponParams = {
+  couponId: string;
+  expiresAt: Date;
+  minimumQuantity: number;
+};
+export class BogoCoupon implements CouponPolicy {
+  couponId;
+  expiresAt;
+  type = 'BOGO' as const;
+  minimumQuantity;
+
+  constructor(params: BogoCouponParams) {
+    this.couponId = params.couponId;
+    this.expiresAt = params.expiresAt;
+    this.minimumQuantity = params.minimumQuantity;
+  }
+
+  isApplicable(context: CouponContext) {
+    const isCountThree = context.orderProducts.some(
+      (product) => product.quantity >= this.minimumQuantity,
+    );
+
+    return context.now <= this.expiresAt && isCountThree;
+  }
+  calculateDiscount(context: CouponContext): CouponDiscount {
+    const targetProducts = context.orderProducts.filter(
+      (product) => product.quantity >= this.minimumQuantity,
+    );
+
+    const highestPrice = Math.max(
+      ...targetProducts.map((product) => product.productPrice),
+    );
+
+    if (targetProducts.length === 0) {
+      return {
+        productDiscountPrice: 0,
+        deliveryDiscountPrice: 0,
+      };
+    }
+    return {
+      productDiscountPrice: highestPrice,
+      deliveryDiscountPrice: 0,
+    };
+  }
+}
+
+type FreeShippingCouponParams = {
+  couponId: string;
+  expiresAt: Date;
+  minimumOrderPrice: number;
+};
+
+export class FreeShippingCoupon implements CouponPolicy {
+  couponId;
+  expiresAt;
+  type = 'FREE_SHIPPING' as const;
+  minimumOrderPrice;
+
+  constructor(params: FreeShippingCouponParams) {
+    this.couponId = params.couponId;
+    this.expiresAt = params.expiresAt;
+    this.minimumOrderPrice = params.minimumOrderPrice;
+  }
+
+  isApplicable(context: CouponContext) {
+    return (
+      context.now <= this.expiresAt &&
+      context.orderPrice >= this.minimumOrderPrice
+    );
+  }
+  calculateDiscount(context: CouponContext): CouponDiscount {
+    return {
+      productDiscountPrice: 0,
+      deliveryDiscountPrice: context.deliveryFee,
+    };
+  }
+}
+
+type MiracleSaleCouponParams = {
+  couponId: string;
+  expiresAt: Date;
+  discountRate: number;
+  startHour: number;
+  endHour: number;
+};
+
+export class MiracleSaleCoupon implements CouponPolicy {
+  couponId;
+  expiresAt;
+  type = 'RATE' as const;
+  discountRate;
+  startHour;
+  endHour;
+
+  constructor(params: MiracleSaleCouponParams) {
+    this.couponId = params.couponId;
+    this.expiresAt = params.expiresAt;
+    this.discountRate = params.discountRate;
+    this.startHour = params.startHour;
+    this.endHour = params.endHour;
+  }
+
+  isApplicable(context: CouponContext) {
+    const currentHour = context.now.getHours();
+
+    return (
+      context.now <= this.expiresAt &&
+      currentHour >= this.startHour &&
+      currentHour < this.endHour
+    );
+  }
+  calculateDiscount(context: CouponContext): CouponDiscount {
+    const totalPrice = context.orderProducts.reduce((sum, product) => {
+      return sum + product.productPrice * product.quantity;
+    }, 0);
+
+    return {
+      productDiscountPrice: totalPrice * this.discountRate,
+      deliveryDiscountPrice: 0,
+    };
+  }
+}
