6878cf06176f0c739f442df3f192d1500c5fea0d
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-06-17 15:47:45 +0900

    test: 쿠폰 API 라우터 테스트 작성 및 구현

--- server/__test__/helpers/testDatabase.ts
+++ server/__test__/helpers/testDatabase.ts
@@ -1,4 +1,5 @@
 import { cartItemsDB, ordersDB, productsDB } from '../../src/db.js';
+import { Order } from '../../src/modules/orders/orders.model.js';
 import { Product } from '../../src/modules/products/product.model.js';
 
 export const resetTestDatabase = () => {
@@ -13,4 +14,13 @@ export const resetOrderDatabase = () => {
 
 export const seedProduct = (productId: string, product: Product) => {
   productsDB.set(productId, product);
+};
+export const seedOrder = (product: Product, quantity: number) => {
+  const mockOrder = new Order({
+    orderId: 'order-1',
+    products: [{ productId: product.productId, quantity }],
+    couponIds: [],
+  });
+
+  ordersDB.set(mockOrder.orderId, mockOrder);
 };
